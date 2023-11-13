c670830cea78f616340de61fc1640ac396b2a005
mirigabe <fvigin@hotmail.com>
2023-11-13 21:07:16 +0100

    refactoring

--- bms/models/maintainance_object.py
+++ bms/models/maintainance_object.py
@@ -24,9 +24,9 @@ class MaintainanceObject(models.Model):
         column1="object_id",
         column2="object_type_id",
     )
-    attr_value_ids = fields.One2many(
-        comodel_name="bms.attribute_value", inverse_name="object_id"
-    )
+    # attr_value_ids = fields.One2many(
+    #     comodel_name="bms.attribute_value", inverse_name="object_id"
+    # )
     parent_object_ids = fields.One2many(comodel_name="bms.decomposition_relationship", inverse_name="parent_object_id")
     decomposition_ids = fields.One2many(
         comodel_name="bms.decomposition_relationship", inverse_name="object_id"
--- bms/models/object_type.py
+++ bms/models/object_type.py
@@ -16,20 +16,7 @@ class ObjectType(models.Model):
     otl_name = fields.Char(related="otl_id.name")
 
     otl_id = fields.Many2one(comodel_name="bms.object_type_library")
-    # attr_value_ids = fields.One2many(
-    #     comodel_name="bms.attribute_value",
-    #     inverse_name="object_type_id"
-    # )
-    oslo_attributen_value_id = fields.One2many(
-        comodel_name="bms.oslo_attributen_value",
-        inverse_name="object_type_id"
-    )
-    # attribute_ids = fields.Many2many(
-    #     comodel_name="bms.attribute_definition",
-    #     relation="bms_attributes_to_types",
-    #     column1="object_type_id",
-    #     column2="attribute_id",
-    # )
+    oslo_attributen_value_id = fields.One2many(comodel_name="bms.oslo_attributen_value", inverse_name="object_type_id")
 
 
     @api.model
