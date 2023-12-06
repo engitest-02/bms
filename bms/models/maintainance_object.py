f878460a147badafcff5a8cddd01b5e45a35d5a2
mirigabe <fvigin@hotmail.com>
2023-12-06 17:58:33 +0100

    [IMP] BMS: inheritance managing organisation

--- bms/models/maintainance_object.py
+++ bms/models/maintainance_object.py
@@ -21,9 +21,9 @@ class MaintainanceObject(models.Model):
     bo_temporary_type = fields.Char(string="proposed new type")
     awv_type_not_found = fields.Boolean(string="AWV type not found?")
 
-    is_managing_level = fields.Boolean(string="Is managing level?" )
-
     ownership_doc = fields.Char(string="link ownership doc")
+
+    is_managing_level = fields.Boolean(string="Is managing level?" ) 
     contract_mgr_lantis = fields.Many2one(comodel_name="bms.agent", string="contract mgr Lantis")
     contract_mgr_contractor = fields.Many2one(comodel_name="bms.agent", string="contract mgr Contractor")
     maintainance_mgr_lantis = fields.Many2one(comodel_name="bms.agent", string="maintainance mgr Lantis")
@@ -31,9 +31,20 @@ class MaintainanceObject(models.Model):
     technical_mgr_lantis = fields.Many2one(comodel_name="bms.agent", string="technical mgr Lantis")
     technical_mgr_contractor = fields.Many2one(comodel_name="bms.agent", string="technical mgr Contractor")
 
-    # #related
+    #related
     owner_contact = fields.Many2one(related="owner_id.contact_person", string="contact person")
     managing_org_contact = fields.Many2one(related="managing_org_id.contact_person", string="contact person")
+
+    # inherited_ownership_doc = fields.Char(related="mg_level_obj_id.ownership_doc", string="link ownership doc")
+    
+    inherited_managing_org_id = fields.Many2one(related="mg_level_obj_id.managing_org_id", string="managing organisation")
+    inherited_managing_org_contact = fields.Many2one(related="inherited_managing_org_id.contact_person", string="contact person")
+    inherited_contract_mgr_lantis = fields.Many2one(related="mg_level_obj_id.contract_mgr_lantis", string="contract mgr Lantis")
+    inherited_contract_mgr_contractor = fields.Many2one(related="mg_level_obj_id.contract_mgr_contractor", string="contract mgr Contractor")
+    inherited_maintainance_mgr_lantis = fields.Many2one(related="mg_level_obj_id.maintainance_mgr_lantis", string="maintainance mgr Lantis")
+    inherited_maintainance_mgr_contractor = fields.Many2one(related="mg_level_obj_id.maintainance_mgr_contractor", string="maintainance mgr Contractor")
+    inherited_technical_mgr_lantis = fields.Many2one(related="mg_level_obj_id.technical_mgr_lantis", string="technical mgr Lantis")
+    inherited_technical_mgr_contractor = fields.Many2one(related="mg_level_obj_id.technical_mgr_contractor", string="technical mgr Contractor")
 
     #compute
     has_object_type_ids = fields.Boolean(compute="_compute_has_object_type_ids" ,store=False) # field for visualisation purpose. Invisibilize object_type_ids in view
@@ -50,7 +61,7 @@ class MaintainanceObject(models.Model):
 
     owner_id = fields.Many2one(comodel_name="bms.organisation", string="owner")
     managing_org_id = fields.Many2one(comodel_name="bms.organisation", string="managing organisation")
-
+    mg_level_obj_id = fields.Many2one(comodel_name="bms.maintainance_object", string="management level object")
 
 
     # @api.depends("object_type_ids")
@@ -78,32 +89,111 @@ class MaintainanceObject(models.Model):
 
     @api.constrains("decomposition_ids")
     def _constraints_decomposition_ids(self):
-        print("constraints decompostion")
         for rec in self:
             if not rec.decomposition_ids:
                 raise ValidationError("You have to assign at least one decomposition")
 
     @api.model
-    def create(self, vals_list):
+    def create(self, vals):
         # create the right value for internal_id
-        print("create maintainace", vals_list)
+        vals = self._update_internal_id(vals)
+        recs = super(MaintainanceObject,self).create(vals)
+        # update mgt_level_object_id accroding to is_managing_level
+        for rec in recs:
+            rec._update_mg_object(rec)    
+        return recs
+                
+    def write(self, vals):
+        if vals.get('is_managing_level') is not None and self.is_managing_level != vals.get('is_managing_level'):
+
+            self._update_mg_object(vals)
+
+        return super(MaintainanceObject, self).write(vals)
+
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super().get_view(view_id, view_type, **options)
+        return res
+    
+    def _update_internal_id(self, vals):
+        """retun vals with the the incremented internal_id before creation"""
+        # print("create maintainace", vals_list)
         max_internal_id_rec = self.env["bms.maintainance_object"].read_group([], ["internal_id:max(internal_id)"], [])
         max_id = max_internal_id_rec[0].get("internal_id")
         if max_id is None:
             max_id = 0
         
-        if type(vals_list) == dict:
+        if type(vals) == dict:
             # assign internal_id
             max_id +=  1
-            vals_list.update(internal_id=max_id)
-            return super(MaintainanceObject,self).create(vals_list)
-        if type(vals_list) == list:
-            for vals in vals_list:
+            vals.update(internal_id=max_id)
+            
+        if type(vals) == list:
+            for vals_item in vals:
                 max_id +=  1
-                vals.update(internal_id=max_id)
+                vals_item.update(internal_id=max_id)
                 print("create maintainace after update list", vals)
-                return super(MaintainanceObject,self).create(vals)
+        
+        return vals
+
+    def _update_mg_object(self, vals):
+        """ when is_managing_level field is True: the managing level object is the object itself and all its children are updated with its id down to the object where is_managing_level is True
+            when is_managing_level field is False: the managing level is the first of the parent objects with is_managing_level on True. If it does not exist then it remains null            
+            Technical note:
+                use of super(...).write to avoid to fall in infinite loop with the orveridden write() method
+        """
+        # if self.is_managing_level != vals.get("is_managing_level"):
+        if vals["is_managing_level"] is True:
+            self._set_mg_level_to_children(self.id)
+            self._set_mg_level_obj_id(None)
+        if vals is None or vals["is_managing_level"] is False: #vals None if created and not filled-in.
+            mg_level_obj_id = self._get_mg_level_from_parent(self.id)
+            self._set_mg_level_to_children(mg_level_obj_id)
+            self._set_mg_level_obj_id(mg_level_obj_id, reset_mgt_fields=True)
+            
+    def _set_mg_level_obj_id(self, mg_level_obj_id, reset_mgt_fields=False):
+        vals = {'mg_level_obj_id': mg_level_obj_id}
+        if reset_mgt_fields:
+            vals = {**vals,
+                'managing_org_id': None,
+                'contract_mgr_lantis': None,
+                'contract_mgr_contractor': None,
+                'maintainance_mgr_lantis': None,
+                'maintainance_mgr_contractor': None,
+                'technical_mgr_lantis': None,
+                'technical_mgr_contractor': None
+            }
+        super(MaintainanceObject, self).write(vals)
                 
-    def get_view(self, view_id=None, view_type='form', **options):
-        res = super().get_view(view_id, view_type, **options)
-        return res
+    def _set_mg_level_to_children(self, mg_level_obj_id):
+        """assign to the children of mg_level_obj_id, the id down to the first object which is also a """
+        children_recs = self._get_children(self.id)
+        if children_recs:
+            for child_rec in children_recs:
+                if child_rec.is_managing_level is False:
+                    self._set_mg_level_obj_id(mg_level_obj_id)
+                    child_rec._set_mg_level_to_children(mg_level_obj_id)
+        else:
+            if self.is_managing_level is False:
+                self._set_mg_level_obj_id(mg_level_obj_id)
+
+    def _get_children(self, object_id):
+        """ Attention: decompostion_type_id is hardcoded!"""
+        domain = [("parent_object_id", "=", object_id), ("decomposition_type_id", "=", 1)] 
+        recs = self.env["bms.decomposition_relationship"].search(domain)
+        return [rec.object_id for rec in recs]
+    
+    def _get_mg_level_from_parent(self, object_id):
+        """get the first occurence among the parent objects of an object where the managing is performed (could be None)
+           Attention: decompostion_type_id is hardcoded! """
+        domain = [("object_id", "=", object_id), ("decomposition_type_id", "=", 1)]
+        rec = self.env["bms.decomposition_relationship"].search(domain)
+        if rec:
+            if rec.parent_object_id.is_managing_level is True:
+                return rec.parent_object_id
+            else:
+                self._get_mg_level_from_parent(rec.parent_object_id.id)
+        else: 
+            return None
+        
+      
+        
